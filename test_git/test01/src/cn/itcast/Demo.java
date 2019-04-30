eb7a4b2b1dd12e9bc36c98771ba3359ba249f3ea
youxu <1073636779@qq.com>
2019-04-30 16:01:36 +0800

    本地导入仓库

---
+++ test_git/test01/src/cn/itcast/Demo.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
+    }
+}
